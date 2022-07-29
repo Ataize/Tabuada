ff908bc4247aaddbc9d7ae4298143409a099e707
Ataize Feitosa <isa.aragao@hotmail.com>
2022-07-29 20:44:30 -0300

    Primeiro commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+function calcula(){
+    let tabuada = document.querySelector('#tabuada tbody');
+    let valorA = parseInt(document.querySelector('#valorA').value);
+    tabuada.innerHTML = '';
+    
+    for (let valorB = 0; valorB <= 10; valorB++) {
+        let resultado = valorA * valorB;
+        let template = ` 
+            <td>${valorA}</td>
+            <td>x</td>
+            <td>${valorB}</td>
+            <td>=</td>
+            <td>${resultado}</td>`;
+    
+        let tr = document.createElement('tr');
+        tr.innerHTML = template;
+        tabuada.append(tr);
+    
+    }
+
+}
+calcula();
+document.querySelector('#valorA').addEventListener('change', calcula);
